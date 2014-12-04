07af93af326bd0f7c63b9a949de1885678b4dd52
Sam Javner <samuel.javner@gmail.com>
2014-12-04 09:49:11 -0600

    Test split categories

--- mmf_tests.py
+++ mmf_tests.py
@@ -1,4 +1,39 @@
 import unittest
 
+import mmf
+
+class TestSplitCategories(unittest.TestCase):
+
+    def test_when_none(self):
+        actual = mmf._split_categories('None')
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_when_none_mixed_caps(self):
+        actual = mmf._split_categories('noNE')
+        expected = []
+        self.assertEqual(actual, expected)
+
+    def test_when_one_category(self):
+        actual = mmf._split_categories('Dessert')
+        expected = ['Dessert']
+        self.assertEqual(actual, expected)
+
+    def test_when_multiple_categories(self):
+        actual = mmf._split_categories('Dessert,Italian,Easy')
+        expected = ['Dessert', 'Italian', 'Easy']
+        self.assertEqual(actual, expected)
+
+    def test_when_multiple_categories_with_space(self):
+        actual = mmf._split_categories('Dessert, Italian, Easy')
+        expected = ['Dessert', 'Italian', 'Easy']
+        self.assertEqual(actual, expected)
+
+    def test_when_multiple_categories_with_more_space(self):
+        actual = mmf._split_categories(' Dessert , Italian , Easy   ')
+        expected = ['Dessert', 'Italian', 'Easy']
+        self.assertEqual(actual, expected)
+
+
 if __name__ == '__main__':
     unittest.main()
